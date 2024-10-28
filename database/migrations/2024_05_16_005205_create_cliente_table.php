ffd3dcdccbbd14640212bd816a6ac044daf63794
lsbrel <lucas.81.gabriel@gmail.com>
2024-10-28 19:30:31 -0300

    🐞 fix: cidade como string ao inves de id

--- database/migrations/2024_05_16_005205_create_cliente_table.php
+++ database/migrations/2024_05_16_005205_create_cliente_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string("complemento", 100)->nullable();
             $table->string("numero", 5);
             $table->string("telefone", 11);
-            $table->foreignId("cidade_id")->references("id")->on("cidade");
+            $table->string("cidade", 50);
             $table->foreignId('usuario_id')->references('id')->on('usuario');
             $table->timestamps();
         });

785ae06bfb497efb44d9aafd829a62099ab655fa
lsbrel <lucas.81.gabriel@gmail.com>
2024-11-09 14:55:07 -0300

    ✨ feat: realtorio de dashboard

--- app/Http/Controllers/Relatorio/RelatorioController.php
+++ app/Http/Controllers/Relatorio/RelatorioController.php
@@ -7,6 +7,7 @@ use App\Enums\ResponseStatus;
 use App\Http\Controllers\Controller;
 use App\Http\Helpers\Response;
 use App\Models\Cliente;
+use App\Models\OrdemServico;
 
 class RelatorioController extends Controller
 {
@@ -15,10 +16,12 @@ class RelatorioController extends Controller
 
         $clientes_ordem_servico = Cliente::clientsWithMostServiceOrder();
         $clientes_equipamentos = Cliente::clientsWithMostEquipments();
+        $greater_one_year = OrdemServico::getGreaterThen1Year();
 
         return Response::send(ResponseCode::Ok, ResponseStatus::Success, 'show-client-success', [
             "clientes_ordem_servico" => $clientes_ordem_servico,
             "clientes_equipamentos" => $clientes_equipamentos,
+            "greater_one_year" => $greater_one_year,
         ]);
     }
 }
